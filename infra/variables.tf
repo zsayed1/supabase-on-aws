471f0029f33c69eec0a77e74b63c4d571b9625b2
Zeshan Sayed <zeshansayed@Zeshans-MacBook-Pro.local>
2025-09-24 10:45:00 -0500

    Update VPC config and module

---
+++ infra/variables.tf
@@ -0,0 +1,19 @@
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "azs" {
+  description = "List of availability zones"
+  type        = list(string)
+}
+
+variable "public_subnets" {
+  description = "Map of public subnet CIDRs keyed by AZ"
+  type        = map(string)
+}
+
+variable "private_subnets" {
+  description = "Map of private subnet CIDRs keyed by AZ"
+  type        = map(string)
+}
